6ca7e0b8189e714a2c052c72178bb9a80382a6b5
whereislima <reach.lima@gmail.com>
2021-03-31 18:57:54 -0700

    completed

--- serial.py
+++ serial.py
@@ -19,4 +19,22 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start=0):
+        """ """
+        self.start = start
+        
+    def __repr__(self):
+        """Returns a string representation"""
+        return f"start={self.start} next={self.next"
+    
+    def generate(self): 
+        """Returns next serial number"""
+        self.next += 1
+        
+
+    def reset(self):
+        """Reset number to start"""
+
+        self.next = self.start
+        
 
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,43 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+
+    def __init__(self, path):
+        """read text file and prints number of words read """
+        dict_file = open(path)
+
+        self.words = self.parse(dict_file)
+    
+        print(f"{len(self.words)} words read")
+
+    def parse(self, dict_file):
+        """returns word without new line"""
+        return [w.strip() for w in dict_file]
+
+    def random(self):
+        """ return random word """
+        return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    """Specialized WordFinder that excludes blank lines/comments.
+    
+    >>> swf = SpecialWordFinder("complex.txt")
+    3 words read
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+    """
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words, skipping blanks/comments."""
+
+        return [w.strip() for w in dict_file
+                if w.strip() and not w.startswith("#")]
